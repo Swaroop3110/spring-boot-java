f48b1a5c73564fe3c62f4786f4884d7bfdc51b40
swaroop3110 <swarooppolani@gmail.com>
2022-04-19 14:14:56 -0400

    First commit

---
+++ src/main/java/gradleproj/demo/PropsApplication.java
@@ -0,0 +1,13 @@
+package gradleproj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gradleproj/demo/PropsApplicationTests.java
@@ -0,0 +1,13 @@
+package gradleproj.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
